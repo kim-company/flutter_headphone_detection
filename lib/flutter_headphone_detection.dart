1c74f65d5d9b4c3b19e754298db4dfdadcfd9c03
Johannes Erschbamer <johannes.erschbamer@me.com>
2019-11-05 10:48:09 +0100

    Init packages

--- lib/flutter_headphone_detection.dart
+++ lib/flutter_headphone_detection.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class FlutterHeadphoneDetection {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_headphone_detection');
+  static const MethodChannel _channel = const MethodChannel('flutter_headphone_detection');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
